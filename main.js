10c12a002e7bccf6c884f55b0925d91358689696
unknown <[camuzetjeremy@gmail.com]>
2022-09-01 13:56:55 +0200

    api pour la meteo

---
+++ main.js
@@ -0,0 +1,43 @@
+let formSubmit = document.getElementById('formSubmit')
+let valueInput = document.getElementById('valueInput')
+let result = document.getElementById('result')
+let city = ""
+let meteo = []
+const ApiKey = 'ad8b8388eceddfc6188e49641dca866d'
+
+const fetchMeteo = async ()=>{
+  const ApiUri = `http://api.weatherstack.com/current?access_key=${ApiKey}&query=${city}`
+  meteo = await fetch(ApiUri).then((response) => response.json())
+  console.log(meteo);
+}
+
+const displayMeteo = async ()=>{
+    await fetchMeteo()
+    result.innerHTML = `
+    <div class="card mx-auto" style="width: 18rem;">
+    <img src="${meteo.current.weather_icons[0]}" class="card-img-top" alt="méteo du jour">
+        <div class="card-body">
+            <h5 class="card-title">${meteo.location.name} ${meteo.location.region} ${meteo.location.country}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${meteo.current.weather_descriptions[0]}</h6>
+            <p class="card-text">temperature : ${meteo.current.temperature}° 🌡️</p>
+            <p class="card-text">humidité : ${meteo.current.humidity}% 💧</p>
+            <p class="card-text">force du vent : ${meteo.current.wind_speed} km/h 🌬️</p>
+            <p class="card-text">index uv : ${meteo.current.uv_index} 🌞</p>
+            <p class="card-text">precipation : ${meteo.current.precip} 🌤️🌥️🌦️🌨️🌩️</p>
+        </div>
+    </div>
+`
+}
+
+formSubmit.addEventListener('submit', e => {
+  e.preventDefault()
+  city = valueInput.value;
+  city.length > 2 ?
+    displayMeteo()
+  :
+    result.innerHTML = `
+    <div class="alert alert-danger col-8 mx-auto" role="alert">
+      Veuillez saisir le nom d'une ville!
+    </div>
+    `
+})
